86e88c02d0478f5586e39ac030576ecbab000b6b
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-02-09 18:25:04 +0100

    9 fix named pipe issue (#11)
    * Add docker command to README.md and modify simulation files so they avoid features we do not have available.
    
    * Rename example folders and add verilator docker scripts
    
    * Remove unecessary code.
    
    * Update action
    
    * Remove interactive docker when not necessary.
    
    * Set shell in makefile
    
    * Try different approach to escape #
    
    * Add comment and fix named pipe issue.

--- verilator.mk
+++ verilator.mk
@@ -10,6 +10,9 @@ HASHTAG := \#
 # Just doing ${MODULE_FOLDER}/${PROJECT_NAME} is not sufficient, because there
 # might be folders between the module folder and project folder. Like in the
 # Hierachy example.
+# 
+# The cryptographic code is extracted from:
+# https://unix.stackexchange.com/questions/392528/extract-sub-directory-path-from-partially-known-directory 
 PATH_FROM_ROOT_TO_SRC = $(shell p=$(shell pwd); g=$${p${HASHTAG}${HASHTAG}*/${MODULE_FOLDER}}; echo $$g)
 
 VERILATOR := $(VERILATOR_ROOT)/verilator-docker.sh
@@ -30,7 +33,9 @@ $(VERILATOR_EMULATOR): lint | $(SOURCES) $(GENERATED)
 
 # https://stackoverflow.com/questions/17757039/equivalent-of-pipefail-in-dash-shell
 run: $(VERILATOR_EMULATOR)
-	@mkfifo named_pipe
+	@if [[ ! -e named_pipe ]]; then \
+		mkfifo named_pipe; \
+	fi
 	@tee output.txt < named_pipe &
 	@./$(VERILATOR_EMULATOR) > named_pipe; ./$(PARSER) $(PROJECT_NAME) $$? output.txt
 	@rm named_pipe
