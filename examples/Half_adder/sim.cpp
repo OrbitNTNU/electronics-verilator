67ef358a1f560703bb216eed35561ac4d0941fe1
Tortrix <jacob.wibe@orbitntnu.com>
2024-02-11 16:56:34 +0100

    Add and modify multiplier and half_adder

--- examples/Half_adder/sim.cpp
+++ examples/Half_adder/sim.cpp
@@ -12,20 +12,17 @@ int main(int argc, char** argv) {
 
     int *operands = new int[200];
     int noperands = get_stimuli(std::string{"operands.data"}, operands);
-    int nlines = noperands / 4;
+    int nlines = noperands / 2;  // Change to 2 since you have two inputs (a and b) per line
 
     for (int i = 0; i < nlines; i++) {
-        top->a1 = operands[i * 4 + 0];
-        top->a2 = operands[i * 4 + 1];
-        top->b1 = operands[i * 4 + 2];
-        top->b2 = operands[i * 4 + 3];
+        top->a = operands[i * 2 + 0];  
+        top->b = operands[i * 2 + 1];  
 
         top->eval();
 
-        assert(top->res1 == (top->a1 - top->b1));
-        assert(top->res2 == (top->a2 - top->b2));
+        assert(top->sum == (top->a ^ top->b));     // Check the output 'sum'
+        assert(top->carry == (top->a & top->b));   // Check the output 'carry'
     }
-
 
     delete top;
     return 0;
