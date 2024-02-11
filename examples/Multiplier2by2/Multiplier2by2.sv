ec9b5f8f6479f9f6395411e081c382be4fa4a910
Tortrix <jacob.wibe@orbitntnu.com>
2024-02-11 17:00:06 +0100

    Add directories with and for half adder,multiplier and calculator

---
+++ examples/Multiplier2by2/Multiplier2by2.sv
@@ -0,0 +1,16 @@
+module Multiplier2by2(Prod,A,B);
+
+    input [1:0] A,B;
+    output [3:0] P;
+    wire w1,w2,w3,C;
+
+    and n1(P[0],A[0],B[0]);
+
+    and n2(w1,A[0],B[1]);
+    and n3(w2,A[1],B[0]);
+    Half_adder n4(P[1],C,w1,w2);
+
+    and n5(w3,A[1],B[1]);
+    Half_adder n6(P[2],P[3],w3,C);
+
+endmodule
---
+++ examples/Multiplier2by2/sim.cpp
@@ -0,0 +1,32 @@
+#include "VMultiplier2by2.h"
+#include "verilated.h"
+
+#include <cassert>
+#include <iostream>
+
+int get_stimuli(std::string filename, int *data);
+
+int main(int argc, char** argv) {
+    VHalf_adder* top = new VHalf_adder;
+    Verilated::commandArgs(argc, argv);
+
+    int *operands = new int[200];
+    int noperands = get_stimuli(std::string{"operands.data"}, operands);
+    int nlines = noperands / 4;
+
+    for (int i = 0; i < nlines; i++) {
+        top->a1 = operands[i * 4 + 0];
+        top->a2 = operands[i * 4 + 1];
+        top->b1 = operands[i * 4 + 2];
+        top->b2 = operands[i * 4 + 3];
+
+        top->eval();
+
+        assert(top->res1 == (top->a1 - top->b1));
+        assert(top->res2 == (top->a2 - top->b2));
+    }
+
+
+    delete top;
+    return 0;
+}
