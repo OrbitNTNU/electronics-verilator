e0729ee1d323e48a6073873901a1ec9323775ed0
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-02-16 20:27:48 +0100

    Add some support for automatically displaying enums in gtkwave.
    The flow is basically like this:
    1. An enum is written by the developer in a .sv file. The developer must
    use verilator directives to generate its equivalent in C++.
    2. The generated enum is parsed in Python. The names of the states and
    corresponding values are extracted.
    3. The names and values are written to a .gtkw save file. This save
    file can be used by the developer to display an enum's strings
    instead of values.
    
    Add a README.md which explains how to use this in a step-by-step guide.

--- verilator.mk
+++ verilator.mk
@@ -10,6 +10,9 @@ HASHTAG := \#
 # Just doing ${MODULE_FOLDER}/${PROJECT_NAME} is not sufficient, because there
 # might be folders between the module folder and project folder. Like in the
 # Hierachy example.
+# 
+# The cryptographic code is extracted from:
+# https://unix.stackexchange.com/questions/392528/extract-sub-directory-path-from-partially-known-directory 
 PATH_FROM_ROOT_TO_SRC = $(shell p=$(shell pwd); g=$${p${HASHTAG}${HASHTAG}*/${MODULE_FOLDER}}; echo $$g)
 
 VERILATOR := $(VERILATOR_ROOT)/verilator-docker.sh
@@ -20,7 +23,7 @@ VERILATOR_ARGS_EMULATOR := --cc --build --exe --trace
 VERILATOR_EMULATOR := obj_dir/V$(PROJECT_NAME)
 PARSER ?= $(VERILATOR_ROOT)/default_parser.sh
 
-all: run
+all:: run
 
 lint: $(SOURCES) $(GENERATED)
 	./$(VERILATOR) $(VERILATOR_ARGS) $(VERILATOR_ARGS_LINT) $(SOURCES) --top-module $(PROJECT_NAME)
@@ -29,12 +32,14 @@ $(VERILATOR_EMULATOR): lint | $(SOURCES) $(GENERATED)
 	./$(VERILATOR) $(VERILATOR_ARGS) $(VERILATOR_ARGS_EMULATOR) $(SIMFILES) $(SOURCES) > /dev/null
 
 # https://stackoverflow.com/questions/17757039/equivalent-of-pipefail-in-dash-shell
-run: $(VERILATOR_EMULATOR)
-	@mkfifo named_pipe
+run:: $(VERILATOR_EMULATOR)
+	@if [[ ! -e named_pipe ]]; then \
+		mkfifo named_pipe; \
+	fi
 	@tee output.txt < named_pipe &
 	@./$(VERILATOR_EMULATOR) > named_pipe; ./$(PARSER) $(PROJECT_NAME) $$? output.txt
 	@rm named_pipe
 
-clean:
+clean::
 	@rm -rf obj_dir
 	@rm -f named_pipe
