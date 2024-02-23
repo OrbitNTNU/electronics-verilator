2991b5025a5afefaa73f6af59460cc71c1688f30
JorgenA <jorgen.aagesen@gmail.com>
2024-02-23 16:40:18 +0100

    Add Led_blinker example

---
+++ examples/Led_blinker/sim.cpp
@@ -0,0 +1,28 @@
+#include "VLed_blinker.h"
+#include "verilated.h"
+
+#include <cassert>
+#include <vector>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    VLed_blinker* top = new VLed_blinker;
+    Verilated::commandArgs(argc, argv);
+    
+    int stimuli_a[] = { 2, -1, 1, -6 };
+    int stimuli_b[] = { 4,  5, 2,  7 }; 
+
+    for (int i = 0; i < sizeof(stimuli_a) / sizeof(stimuli_a[0]); i++) {
+        if (Verilated::gotFinish()) break;
+        
+        top->a = stimuli_a[i];
+        top->b = stimuli_b[i];
+        
+        top->eval();
+
+        assert(top->c == (stimuli_a[i] + stimuli_b[i]));
+    }
+    
+    delete top;
+    return 0;
+}
